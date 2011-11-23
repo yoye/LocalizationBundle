5ee789739096428e753b45a8885f482314158883
yoye <y.aparici@gmail.com>
2011-11-23 11:19:15 +0100

    Add localizer service

---
+++ Localization/Localizer.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Yoye\Bundle\LocalizationBundle\Localization;
+
+use DateTime,
+    IntlDateFormatter;
+use Symfony\Component\Locale\Locale;
+use Symfony\Component\Locale\Stub\StubLocale;
+use Symfony\Component\HttpFoundation\Session;
+
+class Localizer
+{
+
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @var array
+     */
+    private $currencies = array();
+
+    /**
+     * __construct
+     * 
+     * @param Session $session 
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * Localize country name
+     *
+     * @param string $code 
+     * @return string
+     */
+    public function getDisplayCountry($code)
+    {
+        $code = strtoupper($code);
+        $countries = Locale::getDisplayCountries($this->session->getLocale());
+
+        if (isset($countries[$code])) {
+            return $countries[$code];
+        }
+    }
+
+    /**
+     * Get language name
+     * 
+     * @param string $code 
+     * @return string
+     */
+    public function getDisplayLanguage($code)
+    {
+        $languages = Locale::getDisplayLanguages($this->session->getLocale());
+
+        if (isset($languages[$code])) {
+            return $languages[$code];
+        }
+    }
+
+    /**
+     * Get currency symbol
+     * 
+     * @param string $code
+     * @return string
+     */
+    public function getCurrencySymbol($code)
+    {
+        if (empty($this->currencies)) {
+            $this->currencies = StubLocale::getCurrenciesData('en');
+        }
+        
+        if (isset($this->currencies[$code]['symbol'])) {
+            return $this->currencies[$code]['symbol'];
+        }
+    }
+
+    /**
+     * Format date 
+     * 
+     * @param DateTime $date 
+     * @return string
+     */
+    public function getDateIntl(DateTime $date, $dateFormat = IntlDateFormatter::MEDIUM)
+    {
+        $intl = new IntlDateFormatter($this->session->getLocale(), $dateFormat, IntlDateFormatter::NONE);
+
+        return $intl->format($date->getTimestamp());
+    }
+
+    /**
+     * Format time
+     * 
+     * @param DateTime $date 
+     * @return string
+     */
+    public function getTimeIntl(DateTime $date, $timeFormat = IntlDateFormatter::MEDIUM)
+    {
+        $intl = new IntlDateFormatter($this->session->getLocale(), IntlDateFormatter::NONE, $timeFormat);
+
+        return $intl->format($date->getTimestamp());
+    }
+
+}
--- Twig/Extension/LocalizationExtension.php
+++ Twig/Extension/LocalizationExtension.php
@@ -2,27 +2,19 @@
 
 namespace Yoye\Bundle\LocalizationBundle\Twig\Extension;
 
-use DateTime, IntlDateFormatter, NumberFormatter;
-use Symfony\Component\Locale\Locale;
-use Symfony\Component\Locale\Stub\StubLocale;
-use Symfony\Component\HttpFoundation\Session;
+use Yoye\Bundle\LocalizationBundle\Localization\Localizer;
 
 class LocalizationExtension extends \Twig_Extension
 {
 
     /**
-     * @var Session
+     * @var Localizer
      */
-    private $session;
+    private $localizer;
 
-    /**
-     * @var array
-     */
-    private $currencies = array();
-
-    public function __construct(Session $session)
+    public function __construct(Localizer $localizer)
     {
-        $this->session = $session;
+        $this->localizer = $localizer;
     }
 
     public function getFunctions()
@@ -39,7 +31,6 @@ class LocalizationExtension extends \Twig_Extension
         return array(
             'l10n_date' => new \Twig_Filter_Method($this, 'getDateIntl'),
             'l10n_time' => new \Twig_Filter_Method($this, 'getTimeIntl'),
-            'l10n_number_format' => new \Twig_Filter_Method($this, 'getNumberFormat'),
         );
     }
 
@@ -47,93 +38,12 @@ class LocalizationExtension extends \Twig_Extension
     {
         return 'localization_extension';
     }
-
-    /**
-     * Localize country name
-     *
-     * @param string $code 
-     * @return string
-     */
-    public function getDisplayCountry($code)
+    
+    public function __call($method, $arguments)
     {
-        $code = strtoupper($code);
-        $countries = Locale::getDisplayCountries($this->session->getLocale());
-
-        if (isset($countries[$code])) {
-            return $countries[$code];
+        if (is_callable(array($this->localizer, $method))) {
+            return call_user_func_array(array($this->localizer, $method), $arguments);
         }
-    }
-
-    /**
-     * Get language name
-     * 
-     * @param string $code 
-     * @return string
-     */
-    public function getDisplayLanguage($code)
-    {
-        $languages = Locale::getDisplayLanguages($this->session->getLocale());
-
-        if (isset($languages[$code])) {
-            return $languages[$code];
-        }
-    }
-
-    /**
-     * Get currency symbol
-     * 
-     * @param string $code
-     * @return string
-     */
-    public function getCurrencySymbol($code)
-    {
-        if (empty($this->currencies)) {
-            $this->currencies = StubLocale::getCurrenciesData('en');
-        }
-
-        if (isset($this->currencies[$code]['symbol'])) {
-            return $this->currencies[$code]['symbol'];
-        }
-    }
-
-    /**
-     * Format date 
-     * 
-     * @param \DateTime $date 
-     * @return string
-     */
-    public function getDateIntl(DateTime $date, $dateFormat = IntlDateFormatter::MEDIUM)
-    {
-        $intl = new IntlDateFormatter($this->session->getLocale(), $dateFormat, IntlDateFormatter::NONE);
-        
-        return $intl->format($date->getTimestamp());
-    }
-
-    /**
-     * Format time
-     * 
-     * @param \DateTime $date 
-     * @return string
-     */
-    public function getTimeIntl(DateTime $date, $timeFormat = IntlDateFormatter::MEDIUM)
-    {
-        $intl = new IntlDateFormatter($this->session->getLocale(), IntlDateFormatter::NONE, $timeFormat);
-
-        return $intl->format($date->getTimestamp());
     }
     
-    /**
-     * Number formatter
-     * 
-     * @param mixed $value
-     * @param integer $type
-     * @return string
-     */
-    public function getNumberFormat($value, $type = NumberFormatter::DECIMAL)
-    {
-        $formatter = new NumberFormatter($this->session->getLocale(), $type);
-        
-        return $formatter->format($value);
-    }
-
 }
